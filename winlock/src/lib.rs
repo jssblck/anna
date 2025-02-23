64a0420b0d3b2b715c5023c647997e093090dd9b
Jessica Black <me@jessica.black>
2025-02-22 22:26:11 -0800

    chore: convert to MPL-2.0 license

--- winlock/src/lib.rs
+++ winlock/src/lib.rs
@@ -1,3 +1,7 @@
+//! This Source Code Form is subject to the terms of the Mozilla Public
+//! License, v. 2.0. If a copy of the MPL was not distributed with this
+//! file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 //! Winlock is an LLM agent coding assistant named after Anna Winlock.
 //!
 //! Anna Winlock was a pioneering woman in computing who worked at Harvard College
